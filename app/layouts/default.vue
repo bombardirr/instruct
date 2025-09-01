b5f3e0f216635c9d47df88af9af8ab395c6b11fc
ag <artem.golovin1@gmail.com>
2025-09-01 14:52:15 +0300

    initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="default-layout">
+    <header>Header</header>
+    <main>
+      <slot/>
+    </main>
+    <footer>Footer</footer>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>re</div>
+    <div>re</div>
+    <div>re</div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+@import 'tailwindcss';
+
+.container {
+  @apply flex flex-col items-center justify-center;
+
+  div:nth-child(1) {
+    @apply bg-green-300;
+  }
+}
+</style>
